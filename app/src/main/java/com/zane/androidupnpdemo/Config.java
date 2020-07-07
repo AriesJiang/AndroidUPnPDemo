34e8330dd3f6ec8a7ecf0db378880be5ead7178c
蒋毅东 <ariesjiang@kugou.net>
2020-07-07 14:24:16 +0800

    [fix]-修复项目编译问题

--- app/src/main/java/com/zane/androidupnpdemo/Config.java
+++ app/src/main/java/com/zane/androidupnpdemo/Config.java
@@ -12,8 +12,10 @@ public class Config {
     //http://mp4.res.hunantv.com/video/1155/79c71f27a58042b23776691d206d23bf.mp4
     // ts 格式
 //    public static String TEST_URL = "http://ottvideows.hifuntv.com/b36ea6f167c7b5785f3aa46c47b6d983/595f51c1/internettv/c1/2017/03/29/41E0B7C03C15AD472DB008A5FF4566EB.ts?uuid=0c18530ecda4454db49665b178396ff7";
+    // flv 格式
+//    public static String TEST_URL = "http://sdtel.stream.kugou.com/live/fx_hifi_1309321536.flv?token=0814adf3c22c79159a8081f066f421ca&us=0&tp=1594091794000523&ti=30&kd=69&ua=fx-alone-android&cn=fx&cp=2763559799&lr=0&fx-ps=2-0-1594091794525";
     // m3u8 格式
-    public static String TEST_URL = "https://https-play-g3proxy.lecloud.com/vod/v2/MjYwLzM5LzEwNy9iY2xvdWQvMTAwMDAxL3Zlcl8wMF8yMi0xMTAwMzg4MTg5LWF2Yy00MTg4NTgtYWFjLTQ4MDAwLTU1MzYwLTMzMTk4MDYtMjUzY2MwODk0ODgyZDE3OGRiM2VjMjc3OTliMzc5ODYtMTQ5NDM4MzI2NTkzOC5tcDQ=?b=479&mmsid=234064503&tm=1496913122&pip=cbfbd2fac71c5662777858b9b5e8b611&key=d15f73e306619c49145da43a477197e2&platid=2&splatid=209&payff=0&cuid=100001&vtype=13&dur=55&p1=3&p2=31&p3=311&cf=h5-ios&p=101&playid=0&tss=ios&tag=mobile&sign=bcloud_100001&termid=2&pay=0&ostype=ios&hwtype=un&ajax=1";
+    public static String TEST_URL = "rtmp://183.57.150.180:1935/live/fx_hifi_1309321536_low_avc?token=3114f18ebdff028cacb3ace718686300&us=0&tp=1594091794000523&ti=30&kd=69&ua=fx-alone-android&cn=fx&cp=2763559799&lr=0&fx-ps=2-0-1594091794525";
 
     /*** 因为后台给的地址是固定的，如果不测试投屏，请设置为 false*/
     public static final boolean DLAN_DEBUG = true;
